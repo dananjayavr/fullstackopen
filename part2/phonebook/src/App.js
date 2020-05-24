40ca8094b6f5a0d992f559f12eb9041cb38feae9
Dananjaya Ramanayake <dananjaya86@yahoo.com>
2020-05-24 11:22:23 +0200

    2.7: The Phonebook Step2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -12,8 +12,13 @@ const App = () => {
     const latestName = {
       name: newName
     }
-    
-    setPersons(persons.concat(latestName))
+
+    if(persons.find(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat(latestName))
+    }
+
     setNewName('')
   }
 
