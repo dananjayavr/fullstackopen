6641e30582828c2ff349245d0ce4fa5deb63b3d4
Dananjaya Ramanayake <dananjaya86@yahoo.com>
2020-05-24 13:22:14 +0200

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number:'040-123456' }
+    { name: 'Arto Hellas', number:'040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ nameFilter, setNameFilter ] = useState('')
 
   const addNewPerson = (event) => {
     event.preventDefault()
@@ -32,9 +36,17 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleNameFilter = (event) => {
+    setNameFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={handleNameFilter} />
+      </div>
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input onChange={handleAddNewName} value={newName}/>
@@ -48,11 +60,15 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => {
-        return (
+        if(person.name.toLowerCase().includes(nameFilter.toLowerCase())) {
+          return (
           <div key={person.name}>
             <div>{person.name} {person.number}</div>
           </div>
-        )
+          )
+        } else {
+          return false
+        }
       })}
     </div>
   )
