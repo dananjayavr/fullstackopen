b6a56c114ea99534f2f6bed465808cc5b2cc6d52
Dananjaya Ramanayake <dananjaya86@yahoo.com>
2020-05-24 22:45:15 +0200

    2.14*: Data for countries, step3

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -3,6 +3,31 @@ import axios from 'axios'
 
 
 const Country = ({ country }) => {
+
+  const [temperature, setTemperature] = useState(0)
+  const [wind, setWind] = useState('')
+  const [icon, setIcon] = useState('')
+  const [weatherDescription, setWeatherDescription] = useState('')
+
+  const hook = () => {
+    const params = {
+      access_key: process.env.REACT_APP_WEATHERSTACK_API_KEY,
+      query: country.capital
+    }
+
+    axios.get('http://api.weatherstack.com/current', { params })
+      .then(response => {
+        setTemperature(response.data.current.temperature);
+        setWind(response.data.current.wind_speed + ' mph direction ' + response.data.current.wind_dir )
+        setIcon(response.data.current.weather_icons[0])
+        setWeatherDescription(response.data.current.weather_descriptions[0])
+      }).catch(error => {
+        console.log(error);
+      });
+  }
+
+  useEffect(hook,[])
+
   return (
     <>
       <h2>{country.name}</h2>
@@ -12,7 +37,11 @@ const Country = ({ country }) => {
       <ul>
         {country.languages.map((language) => <li key={language.name}>{language.name}</li>)}
       </ul>
-      <img src={country.flag} style={{width: 200 + 'px'}}></img>
+      <img src={country.flag} style={{ width: 200 + 'px' }} alt={country.name + ' flag'}/>
+      <h4>Weather in {country.capital}</h4>
+      <div><strong>temperature:</strong> {temperature} Celsius</div>
+      <img src={icon} style={{ width: 50 + 'px' }} alt={weatherDescription}/>
+      <div><strong>wind:</strong> {wind}</div>
     </>
   )
 }
@@ -53,7 +82,7 @@ const App = () => {
         <div>
           find countries <input value={query} onChange={handleQuery} />
         </div>
-        <Country country={results[0]}/>
+        <Country country={results[0]} />
       </>
     )
   } else if (results.length === 0) {
