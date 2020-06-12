6682f15c20a6e35cd543cab99cbeddb9d6689530
Dananjaya Ramanayake <dananjaya86@yahoo.com>
2020-06-12 21:46:40 +0200

    4.4: helper functions and unit tests, step2

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, currentLikes) => (sum + currentLikes)
+  const blogLikes = blogs.map((blog) => blog.likes)
+  return blogLikes.length === 1 ? blogLikes.find((x) => x) : blogLikes.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
