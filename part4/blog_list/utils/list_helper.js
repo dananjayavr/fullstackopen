f37491939d5b2ac724e75e3255ed6a4c9ea103c7
Dananjaya Ramanayake <dananjaya86@yahoo.com>
2020-06-13 02:01:06 +0200

    4.7*: helper functions and unit tests, step5

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -13,20 +13,42 @@ const totalLikes = (blogs) => {
 const favoriteBlog = (blogs) => {
   const blogLikes = blogs.map((blog) => blog.likes)
   const bestLovedIndex = blogLikes.indexOf(Math.max(...blogLikes))
-  return { title: blogs[bestLovedIndex].title, author:blogs[bestLovedIndex].author, likes: blogs[bestLovedIndex].likes }
+  return { title: blogs[bestLovedIndex].title, author: blogs[bestLovedIndex].author, likes: blogs[bestLovedIndex].likes }
 }
 
 const mostBlogs = (blogs) => {
-  const authors = _.groupBy(blogs,'author')
-  const mostBlogsAuthor = _.orderBy(authors,['desc']).pop()
+  const authors = _.groupBy(blogs, 'author')
+  const mostBlogsAuthor = _.orderBy(authors, ['desc']).pop()
   return {
     author: mostBlogsAuthor[0].author,
     blogs: mostBlogsAuthor.length
   }
 }
+
+const mostLikes = (blogs) => {
+
+  const authors = _.groupBy(blogs, 'author')
+  const authorNames = Object.keys(authors)
+  const authorsLikes = Object.create({})
+
+  authorNames.forEach(name => {
+    authorsLikes[name] = _.sumBy(authors[name], (o) => o.likes)
+  })
+
+  const mostLikes = Math.max(...Object.values(authorsLikes))
+  const mostLikesAuthor = Object.keys(authorsLikes).find(key => authorsLikes[key] === mostLikes)
+
+  const response = Object.create({})
+  response['author'] = mostLikesAuthor
+  response['likes'] = mostLikes
+
+  return response
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
-  mostBlogs
+  mostBlogs,
+  mostLikes
 }
