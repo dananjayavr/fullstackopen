475dcb4dc25aa3c1791dac1e8b46c91999f9063a
Dananjaya Ramanayake <dananjaya86@yahoo.com>
2020-06-14 22:28:11 +0200

    4.8: Blog list tests, step1

---
+++ part4/blog_list/tests/blogs_db.test.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('../utils/list_helper')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+
+test('getting all blog entries', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type',/application\/json/)
+})
+
+test('there are 6 notes', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
--- part4/blog_list/utils/config.js
+++ part4/blog_list/utils/config.js
@@ -3,6 +3,10 @@ require('dotenv').config() // To be imported before any other module
 let PORT =  process.env.PORT
 let MONGODB_URI = process.env.MONGO_URI
 
+if(process.env.NODE_ENV === 'test') {
+  MONGODB_URI = process.env.TEST_MONGODB_URI
+}
+
 module.exports = {
   MONGODB_URI,
   PORT
